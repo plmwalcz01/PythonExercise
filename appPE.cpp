dbca690f4b4f42a84f535624222cff314ba89af8
Michał Walczak <michal.walczak@harman.com>
2016-10-11 22:56:58 +0200

    added basic app to be developed later on
    Signed-off-by: Michał Walczak <michal.walczak@harman.com>

---
+++ appPE.cpp
@@ -0,0 +1,19 @@
+
+/********************************************************
+*                                                       *
+*  Project: PythonExercise                              *
+*  Author: Michał Walczak                               *
+*  Version: "1.0"                                       *
+*  Last modified: 2016.10.11                            *
+*  Status: initial draft                                *
+*                                                       *
+********************************************************/
+
+#include <iostream>
+
+
+int main(int argc, char* argv[])
+{
+	std::cout << "initial draft";
+	return 1;
+}
